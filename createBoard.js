93f22fdc10f3f030a6f05eec93cdd91082ae333d
Heidi & Ollie Redding <pair+Wishmouse.oliver-jk-redding@devbootcamp.com>
2016-03-18 12:47:09 +1300

    createBoard

--- createBoard.js
+++ createBoard.js
@@ -1,5 +1,12 @@
 function createBoard(size) {
+ var board = []
+  for(var row = 0; row < size ; row++){
+       board.push([])
+    for (var col =0; col < size; col++){
+      board[row].push(0)
+    }
+  }
+  return board
 }
-
 module.exports = createBoard
 
