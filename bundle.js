66176ad752390560d147b51b7221d092efd8aadd
Ollie Redding <oliver.jk.redding@gmail.com>
2016-03-19 19:35:33 +1300

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -70,7 +70,6 @@ function getNeighbours(cellRow, cellColumn, board) {
   var neighbours = []
   for(var x = cellRow-1; x <= cellRow+1; x++) {
     for(var y = cellColumn-1; y <= cellColumn+1; y++) {
-    	console.log(board)
       if(!indicesOutOfBounds(x, y, board[0]) && board[x][y] != false) {
         if(x != cellRow || y != cellColumn) {
           neighbours.push(board[x][y])
