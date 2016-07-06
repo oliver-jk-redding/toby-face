3967f739b0ab301bbfc506cf83ac4a9f9fac8fc0
oliver-jk-redding <oliver.jk.redding@gmail.com>
2016-07-07 10:11:00 +1200

    add background image

--- conway.js
+++ conway.js
@@ -3,7 +3,7 @@ var nextCellState = require('./nextCellState')
 var createBoard = require('./createBoard')
 var nextBoard = require('./nextBoard')
 
-var board = createBoard(25)
+var board = createBoard(20)
 var nb = nextBoard(board)
 
 setInterval(function() {
